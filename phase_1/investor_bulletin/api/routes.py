52e64594b4a95ef0d27bd02e9c0d2a3ad250aea6
Mohammed Balhaddad <mbailhadadd@gmail.com>
2023-12-16 07:48:10 +0300

    phase_1 init

--- phase_1/investor_bulletin/api/routes.py
+++ phase_1/investor_bulletin/api/routes.py
@@ -1,5 +1,5 @@
-# from api.controllers.market.controllers import router as MarketRouter
+from api.controllers.market_controllers import router as MarketRouter
 
 def init_routes(app):
-    # app.include_router(MarketRouter, prefix="/market-prices", tags=["Market"])
-    # return app
+    app.include_router(MarketRouter, prefix="/market-prices", tags=["Market"])
+    return app
--- phase_1/investor_bulletin/resources/market/market_service.py
+++ phase_1/investor_bulletin/resources/market/market_service.py
@@ -5,5 +5,5 @@ this file to write any business logic for the Market
 
 
 
-# def get_market_data():
-#     return {}
+def get_market_data():
+    return {'Test': 'Test'}
