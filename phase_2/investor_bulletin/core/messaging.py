5e52e01cc80ad780d92e761280e578ecf9b10f32
Mohammed Balhaddad <mbailhadadd@gmail.com>
2023-12-24 09:42:06 +0300

    Finish Phase 2

--- phase_2/investor_bulletin/core/messaging.py
+++ phase_2/investor_bulletin/core/messaging.py
@@ -1,7 +1,20 @@
-# from amqpstorm import Connection
-# Create a connection object to publish events
-# broker = Connection()
+from amqpstorm import Connection
+from amqpstorm import Message
 
+if __name__ == "__main__":
+    # Create a connection object to publish events
+    broker = Connection("localhost", "guest", "guest")
 
-# if __name__ == "__main__":
-    # broker.publish()
+    # Create a Channel
+    channel = broker.channel()
+
+    # Declare the Queue, 'simple_queue'.
+    channel.queue.declare('prices')
+
+    # Message Properties.
+    properties = { 'content_type': 'text/plain' }
+
+    # Create the message.
+    message = Message.create(channel, 'THRESHOLD_ALERT', properties)
+    message.publish("prices")
+    broker.close()
--- phase_2/investor_bulletin/event_subscriber/main.py
+++ phase_2/investor_bulletin/event_subscriber/main.py
@@ -1,12 +1,16 @@
-# from pika import BlockingConnection
+from pika import BlockingConnection,ConnectionParameters
 # Create a connection object to start consuming events
 
-# def init_subscriber()
-  # return BlockingConnection(..)
+def init_subscriber():
+  return BlockingConnection(ConnectionParameters(host='localhost',port=5672))
 
-# def on_event(ch, method, properties, body):
-      # pass
+def on_event(ch, method, properties, body):
+      print(f"Received new message {body}")
 
-# if __name__ == "__main__":
-    # subscriber = init_subscriber()
-    # .basic_consume(queue=queue_name, on_message_callback=on_event)
+if __name__ == "__main__":
+    subscriber = init_subscriber()
+    channel = subscriber.channel()
+    channel.queue_declare(queue='prices')
+    channel.basic_consume(queue='prices', auto_ack=True ,on_message_callback=on_event)
+    print("Start Consuming")
+    channel.start_consuming()
